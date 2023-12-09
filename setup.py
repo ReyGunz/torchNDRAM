c20de55aaa9ac3ea8b53804302c6c76bea0b9b8e
ReyGunz <102969051+ReyGunz@users.noreply.github.com>
2023-12-09 16:22:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
           'torch',
           'tqdm',
           'numpy',
-          'time',
-          'pickle'
+          'time'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
