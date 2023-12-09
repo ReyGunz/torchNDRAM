5275cf6608038e4f3678ccb62d06aa4a511d8232
ReyGunz <102969051+ReyGunz@users.noreply.github.com>
2023-12-09 16:28:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'TORCHNDRAM',         # How you named your package folder (MyLib)
   packages = ['TORCHNDRAM'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'PYTORCH IMPLEMENATION OF CHARTIER\'S AND PROULX\'S NDRAM',   # Give a short description about your library
   author = 'REY A. GONZALEZ',                   # Type in your name
